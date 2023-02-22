d4fd1673fee422226df0edd51b39c8f3e84d263e
mac <mac@macs-MacBook-Air.local>
2023-02-22 14:17:57 -0800

    added a few lines to Rock-Paper-Scissors-Lizard-Spock.py

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
-from game import Game
+from human import Human
 
 class Game:
-    players = int(input("How many players are going to play?"))
+    # players = int(input("How many players are going to play?"))
 
+    def __init__(self) -> None:
+        self.player1 = Human(input("Enter username: "))
+        pass
 
+    def
--- human.py
+++ human.py
@@ -1,16 +1,17 @@
 from player import Player
 
 class Human(Player):
-    
+    def __init__(self, name):
+        super().__init__(name)
+
     def choose_gesture(self):
         print(self.gestures[0])
         #how do we get user input
         #how do we pull value from a list (self.gesture)
         #how do we comebine the previous two points and store chosen gesture
-        username = input("Enter username: ")
-        print("Username is " + username)
+       
 
-        
-Player1 = Human("Bob")
-Player1.choose_gesture()
+
+
+
 
