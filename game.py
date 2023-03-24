33ef8356a3396306464233d12702b03e57e546f7
mac <mac@macs-MacBook-Air.local>
2023-03-24 14:51:37 -0700

    added lines to game.py

--- game.py
+++ game.py
@@ -20,6 +20,16 @@ class Game:
     def run_game(self):
         self.choose_game_type()
         self.best_of_three()
+        self.print_winner()
+        # print winner
+        pass
+
+    def print_winner(self):
+        if self.player1.score == 3:
+            print(self.player1.name + " Is the winner! ")
+
+        elif self.player2.score == 3:
+            print(self.player2.name + "Is the winner! ") 
         pass
 
     def best_of_three(self):
@@ -28,80 +38,90 @@ class Game:
             # p2 choose
             self.player2.choose_gesture()
             # if tie
-            if self.player1.chosen_gesture == "Rock" and self.player2.chosen_gesture == "Rock":
+            if self.player1.chosen_gesture ==  self.player2.chosen_gesture:
                 print("It's a tie")
 
-            elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Paper":
-                print("It's a tie")
-
-            elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Scissors":
-                print("It's a tie")
-
-            elif self.player1.chosen_gesture == "Lizard" and self.player2.chosen_gesture == "Lizard":
-                print("It's a tie")
-
-            elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Spock":
-                print("It's a tie")
-
+            # PLAYER 1 WINS LOGIC
             elif self.player1.chosen_gesture == "Rock" and self.player2.chosen_gesture == "Scissors":
                 print("Rock crushes Scissors")
-
-            elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Paper":
-                print("Scissors cuts Paper")
-
-            elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Rock":
-                print("Paper covers Rock")
+                self.player1.score += 1
 
             elif self.player1.chosen_gesture == "Rock" and self.player2.chosen_gesture == "Lizard":
                 print("Rock crushes Lizard")
-
-            elif self.player1.chosen_gesture == "Lizard" and self.player1.chosen_gesture == "Spock":
-                print("Lizard poisons Spock")
-
-            elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Scissors":
-                print("Spock smashes Scissors")
-
+                self.player1.score += 1
+            
+            elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Paper":
+                print("Scissors cuts Paper")
+                self.player1.score += 1
+            
             elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Lizard":
                 print("Scissors decapitate Lizard")
+                self.player1.score += 1
 
-            elif self.player1.chosen_gesture == "Lizard" and self.player2.chosen_gesture == "Paper":
-                print("Lizard eats Paper")
+            elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Rock":
+                print("Paper covers Rock")
+                self.player1.score += 1
 
             elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Spock":
                 print("Paper disproves Spock")
+                self.player1.score += 1
+
+            elif self.player1.chosen_gesture == "Lizard" and self.player2.chosen_gesture == "Spock":
+                print("Lizard poisons Spock")
+                self.player1.score += 1
+
+            elif self.player1.chosen_gesture == "Lizard" and self.player2.chosen_gesture == "Paper":
+                print("Lizard eats Paper")
+                self.player1.score += 1
+
+            elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Scissors":
+                print("Spock smashes Scissors")
+                self.player1.score += 1
 
             elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Rock":
                 print("Spock vaporizes Rock")
+                self.player1.score += 1
 
+            # PLAYER 2 WINS LOGIC
             elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Rock":
                 print("Rock crushes Scissors")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Scissors":
                 print("Scissors cuts Paper")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Rock" and self.player2.chosen_gesture == "Paper":
                 print("Paper covers Rock")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Lizard" and self.player1.chosen_gesture == "Rock":
                 print("Rock crushes Lizard")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Lizard":
                 print("Lizard poisons Spock")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Scissors" and self.player2.chosen_gesture == "Spock":
                 print("Spock smashes Scissors")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Lizard" and self.player2.chosen_gesture == "Scissors":
                 print("Scissors decapitate Lizard")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Paper" and self.player2.chosen_gesture == "Lizard":
                 print("Lizard eats Paper")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Spock" and self.player2.chosen_gesture == "Paper":
                 print("Paper disproves Spock")
+                self.player2.score += 1
 
             elif self.player1.chosen_gesture == "Rock" and self.player2.chosen_gesture == "Spock":
                 print("Spock vaporizes Rock")
+                self.player2.score += 1
 
             
             # ~20 elifs
